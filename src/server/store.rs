53be21d3053a2cc58368350a0583ba32c054bc87
evan slack <evan.slack@outlook.com>
2025-11-29 17:11:43 -0500

    get store handler

--- src/server/store.rs
+++ src/server/store.rs
@@ -1,4 +1,5 @@
 use axum::{http::StatusCode, routing::get, Router};
+use tracing::{debug, warn};
 
 async fn store() -> StatusCode {
     return StatusCode::NO_CONTENT;
@@ -7,4 +8,16 @@ async fn store() -> StatusCode {
 pub fn create_router() -> Router {
     let router: Router = Router::new().route("/store", get(store));
     return router;
+}
+
+#[debug_handler]
+async fn get_store_jobs(
+    State(state): State<Dependencies>,
+) -> Result<Json<Vec<jobs::StoreJob>>, ServerError> {
+    debug!(route = "/jobs/store", method = "GET", "handle request");
+
+    // fetch store jobs from db
+    let jobs = state.db.get_store_jobs().await?;
+
+    return Ok(Json(jobs));
 }
