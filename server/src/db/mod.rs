ae075d3c0609634840683807e9843541cdfe14f7
evan slack <evan.slack@outlook.com>
2025-12-07 15:48:00 -0500

    export chunks

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -3,5 +3,8 @@ pub mod inmem;
 pub mod jobs;
 pub mod postgres;
 
+pub use chunks::{
+    ChunkMetadata, ChunkMetadataError, ChunkMetadataStore, CreateChunkMetadata, ListChunksQuery,
+};
 pub use jobs::{DynStorer, JobStoreError, JobStorer, LoadJobStorer, StoreJobStorer};
 pub use postgres::PostgresStore;
