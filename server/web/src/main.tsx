4ac4ece3f38d3627d0a1409235a29c33757acb5f
evan slack <evan.slack@outlook.com>
2025-12-18 23:50:06 -0500

    fix app import

--- server/web/src/main.tsx
+++ server/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
+import App from "./app";
 import "./styles/globals.css";
 
 const queryClient = new QueryClient({
